3fb1fa703ada69f0047eb509e0c086283e9bbc1e
Elijah Johnson <ejohnson5@cs.unm.edu>
2019-02-16 17:27:05 -0700

    Added comments and utility functions

--- src/scrabble/TileManager.java
+++ src/scrabble/TileManager.java
@@ -1,3 +1,9 @@
+/**
+ * @author Elijah Johnson
+ * @description Handles keeping track of the tiles that are
+ * left and drawing tiles from the bag randomly
+ */
+
 package scrabble;
 
 import java.io.BufferedReader;
@@ -10,26 +16,42 @@ public class TileManager {
     private List<Tile> bag;
     private Map<Character, Integer> valueMap;
     private Map<Character, Integer> countMap;
-    private List<Character> keys;
 
+    /**
+     * Initialize all values to new instances
+     */
     public TileManager() {
         bag = new ArrayList<>();
         valueMap = new HashMap<>();
         countMap = new HashMap<>();
-        keys = null;
     }
 
+    /**
+     * Initialize the tile manager based
+     * on a file containing the letter distribution to be used
+     * @param letterDist the file that contains the letter
+     *                   distribution to be used
+     * @return true if it initializes correctly otherwise false
+     */
     public boolean initialize(File letterDist) {
         try {
+            //Create buffered reader
             BufferedReader br = new BufferedReader(new FileReader(letterDist));
             String line;
+            //Read line by line
+            //First char represents value
             while ((line = br.readLine()) != null) {
                 String[] array = line.split(" ");
-                int value = Integer.parseInt(line.substring(0, line.indexOf(":")));
+                int value = Integer.parseInt(line.substring(0,
+                        line.indexOf(":")));
+                //Inside { represent count and which letters have that count
                 for (int i = 1; i < array.length; i++) {
-                    int count = Integer.parseInt(array[i].substring(1, array[i].indexOf(":")));
-                    String[] chars = array[i].substring(array[i].indexOf(":") + 1).split(",");
+                    int count = Integer.parseInt(
+                            array[i].substring(1, array[i].indexOf(":")));
+                    String[] chars = array[i].
+                            substring(array[i].indexOf(":") + 1).split(",");
                     for (String s : chars) {
+                        //Add to the count map and value map
                         countMap.put(s.charAt(0), count);
                         valueMap.put(s.charAt(0), value);
                     }
@@ -39,39 +61,72 @@ public class TileManager {
             System.out.println("Cannot read the letter dist file");
             return false;
         }
-
+        //Create the bag
         for (Map.Entry<Character, Integer> entry : countMap.entrySet()) {
             for (int i = 0; i < entry.getValue(); i++) {
                 bag.add(new Tile(entry.getKey(), valueMap.get(entry.getKey())));
             }
         }
-        keys = new ArrayList<>(countMap.keySet());
         return true;
     }
 
+    /**
+     * Gets the value of a string
+     * @param string the string to get the value of
+     * @return the value of string if all the letter values were added together
+     */
     public int getValue(String string) {
         int sum = 0;
         for (int i = 0; i < string.length(); i++) {
             sum += getTileValue(string.charAt(i));
         }
-
         return sum;
     }
 
+    /**
+     * Gets a single character value
+     * @param character the character to get the value of
+     * @return 0 or the value in the value map
+     */
     public int getTileValue(char character) {
-        return valueMap.get(character);
+        return valueMap.getOrDefault(character, 0);
     }
 
+    /**
+     * Draws a new hand of seven tiles
+     * @return the newly drawn list of tiles
+     */
     public List<Tile> drawTray() {
         Random r = new Random();
         List<Tile> tray = new ArrayList<>();
         for (int i = 0; i < 7; i++) {
-            char key = keys.get(r.nextInt(keys.size()));
-            tray.add(new Tile(key, valueMap.get(key)));
-            int newCount = countMap.get(key) - 1;
-            countMap.put(key, newCount);
+            //Draw each tile randomly
+            int next = r.nextInt(bag.size());
+            Tile toGet = bag.get(next);
+            tray.add(toGet);
         }
-
         return tray;
+    }
+
+    /**
+     * Draw one random tile from the bag
+     * @return the random tile from the bag
+     */
+    public Tile drawOne() {
+        Random r = new Random();
+        return bag.get(r.nextInt(bag.size()));
+    }
+
+    /**
+     * Redraw a new tray of tiles, puts the tiles
+     * from the tray back into the bag
+     * @param tray the tray to put back
+     * @return the newly drawn tray
+     */
+    public List<Tile> redrawTray(List<Tile> tray) {
+        List<Tile> newTray = drawTray();
+        bag.addAll(tray);
+        tray.clear();
+        return newTray;
     }
 }
--- src/scrabble/Tray.java
+++ src/scrabble/Tray.java
@@ -1,3 +1,9 @@
+/**
+ * @author Elijah Johnson
+ * @description Handles everything to do with
+ * tiles in a players hand
+ */
+
 package scrabble;
 
 import java.util.ArrayList;
@@ -6,15 +12,35 @@ import java.util.List;
 public class Tray {
     private List<Tile> tiles;
 
+    /**
+     * Initialize the tiles to a new array list
+     */
     public Tray() {
         tiles = new ArrayList<>();
     }
 
+    /**
+     * Set the tiles to a new list of tiles
+     * @param tiles the new list of tiles
+     */
     public void setTiles(List<Tile> tiles) {
         this.tiles = tiles;
     }
 
+    /**
+     * Gets the current tiles in the tray
+     * @return the tiles
+     */
+    public List<Tile> getTiles() {
+        return tiles;
+    }
 
+    /**
+     * Check if a character is in the tray
+     * @param c the character to check is in the tray
+     * @return true if there is a tile in the tray that has the character c
+     * otherwise false
+     */
     public boolean isInTray(char c) {
         for (Tile t : tiles) {
             if (t.getCharacter() == c || t.isBlank()) {
@@ -24,6 +50,51 @@ public class Tray {
         return false;
     }
 
+    /**
+     * Get the size of the tray
+     * should never be greater than seven
+     * @return the tiles.size()
+     */
+    public int size() {
+        return tiles.size();
+    }
+
+    /**
+     * Draw back up to seven after making a play
+     * @param manager the tile manager used
+     *                to draw new tiles
+     */
+    public void redrawToSeven(TileManager manager) {
+        int size = tiles.size();
+        for (int i = size; i < 7; i++) {
+            tiles.add(manager.drawOne());
+        }
+    }
+
+    /**
+     * Removes all of the tiles of toRemove
+     * from tiles
+     * @param toRemove the tiles to remove from the list
+     */
+    public void removeAll(List<Tile> toRemove) {
+        int j;
+        //Make sure to not remove duplicates
+        for (int i = 0; i < size(); i++) {
+            for (j = 0; j < toRemove.size(); j++) {
+                if (toRemove.get(j).getCharacter() == tiles.get(i).getCharacter()) {
+                    tiles.remove(i);
+                    break;
+                }
+            }
+            toRemove.remove(j);
+        }
+    }
+
+    /**
+     * Removes a single tile with the value of c
+     * @param c the character of the tile to remove
+     * @return the removed tile
+     */
     public Tile removeFromTray(char c) {
         Tile toRemove = null;
         for (Tile t : tiles) {
@@ -36,7 +107,16 @@ public class Tray {
         return toRemove;
     }
 
+    /**
+     * Add a tile to the tray
+     * @param t the tile to be added
+     */
     public void addToTray(Tile t) {
         tiles.add(t);
+    }
+
+    @Override
+    public String toString() {
+        return tiles.toString();
     }
 }

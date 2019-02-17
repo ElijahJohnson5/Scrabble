f878601f14d5f56a945f0dc95c3c422642aefb16
Elijah Johnson <ejohnson5@cs.unm.edu>
2019-02-16 23:08:45 -0700

    Updated display

--- src/scrabble/ScrabbleDisplay.java
+++ src/scrabble/ScrabbleDisplay.java
@@ -1,13 +1,37 @@
+//TODO Comment
 package scrabble;
 
 import javafx.fxml.FXML;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.geometry.Insets;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 
 public class ScrabbleDisplay {
 
-    @FXML public HBox computerHand;
-    @FXML public VBox board;
-    @FXML public HBox playerHand;
+    private @FXML HBox computerHand;
+    private @FXML VBox board;
+    private @FXML HBox playerHand;
+
+    public HBox getComputerHand() {
+        return computerHand;
+    }
+
+    public VBox getBoard() {
+        return board;
+    }
+
+    public HBox getPlayerHand() {
+        return playerHand;
+    }
+
+    public void initialize() {
+        board.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+        computerHand.setBackground(new Background(new BackgroundFill(Color.LIGHTSLATEGRAY, CornerRadii.EMPTY, Insets.EMPTY)));
+        playerHand.setBackground(new Background(new BackgroundFill(Color.LIGHTSLATEGRAY, CornerRadii.EMPTY, Insets.EMPTY)));
+    }
 
 }
+
+
+
+

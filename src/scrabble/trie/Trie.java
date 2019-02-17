ba1fdaf7df887309501123c6426a36806ac82541
Elijah Johnson <ejohnson5@cs.unm.edu>
2019-02-16 17:23:48 -0700

    Added comments to trie

--- src/scrabble/trie/Trie.java
+++ src/scrabble/trie/Trie.java
@@ -1,3 +1,10 @@
+/**
+ * @author Elijah Johnson
+ * @description Trie implementation
+ * just like dog, less complicated, takes up
+ * a lot more memory
+ */
+
 package scrabble.trie;
 
 import scrabble.Dictionary;
@@ -5,44 +12,71 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.*;
 
 public class Trie extends Dictionary {
     private int nodeId;
 
+    /**
+     * Create root node with an id of 0
+     */
     public Trie() {
         nodeId = 0;
         root = new TrieNode(nodeId++);
     }
 
+    /**
+     * Inserts a list of words in a file into the dictionary
+     * @param dict the file containing the dictionary to be stored
+     *             int the trie
+     */
     @Override
     public void insert(File dict){
+        List<String> words = new ArrayList<>();
         try {
             BufferedReader br = new BufferedReader(new FileReader(dict));
             String word;
+            //Read each word and make it uppercase
             while ((word = br.readLine()) != null) {
                 word = word.toUpperCase();
-                insert(word);
+                words.add(word);
             }
+            //Sort the words
+            Collections.sort(words);
+            br.close();
         } catch (IOException e) {
             System.out.println("Could not read dictionary file");
         }
-    }
-
-    private void insert(String word) {
-        if (word.length() > 0) {
-            TrieNode temp = (TrieNode) root;
-            for (int i = 0; i < word.length(); i++) {
-                if (!temp.getCharacterNodeMap().containsKey(word.charAt(i))) {
-                    temp.getCharacterNodeMap().put(word.charAt(i), new TrieNode(nodeId++));
-                }
-
-                temp = (TrieNode) temp.getCharacterNodeMap().get(word.charAt(i));
-            }
-
-            temp.setWord(true);
+        //Actually insert each word in the trie
+        for (String word : words) {
+            insert(word);
         }
+        //Make sure the root is not a word
+        root.setWord(false);
     }
 
+    /**
+     * Inserts a word into the trie
+     * @param word the word to be inserted
+     */
+    private void insert(String word) {
+        TrieNode temp = (TrieNode) root;
+        for (int i = 0; i < word.length(); i++) {
+            //If it doesnt contain the key, add it
+            if (!temp.getCharacterNodeMap().containsKey(word.charAt(i))) {
+                temp.getCharacterNodeMap().put(word.charAt(i), new TrieNode(nodeId++));
+            }
+            //Walk down the nodes
+            temp = (TrieNode) temp.getCharacterNodeMap().get(word.charAt(i));
+        }
+        temp.setWord(true);
+    }
+
+    /**
+     * Searches for a word in the dictionary
+     * @param word the word to check is in the dictionary
+     * @return true if it is a word, otherwise false
+     */
     @Override
     public boolean search(String word) {
         TrieNode temp = (TrieNode)root;
@@ -56,11 +90,10 @@ public class Trie extends Dictionary {
         return (temp != null && temp.isWord());
     }
 
-    @Override
-    public TrieNode getRootNode() {
-        return (TrieNode)root;
-    }
-
+    /**
+     * To string used for debugging
+     * @return the root node to string
+     */
     @Override
     public String toString() {
         return root.toString();

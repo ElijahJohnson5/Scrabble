b0410abd948cdc52bca2028c2209998e886a49c8
Elijah Johnson <ejohnson5@cs.unm.edu>
2019-02-16 17:26:31 -0700

    Started scrabble gui

---
+++ src/scrabble/ScrabbleDisplay.java
@@ -0,0 +1,13 @@
+package scrabble;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class ScrabbleDisplay {
+
+    @FXML public HBox computerHand;
+    @FXML public VBox board;
+    @FXML public HBox playerHand;
+
+}
